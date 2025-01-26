d7a77b21dfe761cba140c90c72895941439ad763
paul2803-byte <feichtenschlager10@icloud.com>
2025-01-26 16:42:28 +0100

    Structure for generic Generalization

--- src/main/kotlin/eu/ownyourdata/anonymisationservice/anonymiser/GeneralizationAddress.kt
+++ src/main/kotlin/eu/ownyourdata/anonymisationservice/anonymiser/GeneralizationAddress.kt
@@ -1,9 +1,15 @@
 package eu.ownyourdata.anonymisationservice.anonymiser
 
-class GeneralizationAddress: Generalization() {
+import java.util.*
 
-    override fun anonymise(values: MutableList<Any>): List<Any> {
-        // TODO implement generalization for Address
-        return values
+class GeneralizationAddress: Generalization<String>() {
+    override fun convertValues(index: Int, value: Any): Pair<Int, String> {
+        return Pair(index, "Test Value")
+    }
+
+    override fun getQuantileValues(numericValues: SortedMap<Int, Pair<IntArray, List<String>>>): Map<String, IntArray> {
+        return numericValues.map {(key, value) ->
+            key.toString() to value.first
+        }.toMap()
     }
 }
--- src/main/kotlin/eu/ownyourdata/anonymisationservice/anonymiser/GeneralizationNumeric.kt
+++ src/main/kotlin/eu/ownyourdata/anonymisationservice/anonymiser/GeneralizationNumeric.kt
@@ -2,51 +2,26 @@ package eu.ownyourdata.anonymisationservice.anonymiser
 import java.lang.IllegalArgumentException
 import java.util.*
 
-class GeneralizationNumeric: Generalization() {
+class GeneralizationNumeric: Generalization<Double>() {
 
-    override fun anonymise(values: MutableList<Any>): List<Any> {
-        val bucketNumber = calculateNumberOfBuckets(values.size)
-        val quantiles: SortedMap<Double, Pair<DoubleArray, DoubleArray>> =  values.mapIndexed { index, it ->
-                when (it) {
-                    is Double -> doubleArrayOf(it, index.toDouble())
-                    is Int -> doubleArrayOf(it.toDouble(), index.toDouble())
-                    else -> throw IllegalArgumentException("Numeric Generalization was requested but the input $it is not numeric")
-                }
-            }.sortedBy { v -> v[0] }
-            .mapIndexed { index, it -> it + calculateQuantile(values.size, bucketNumber, index).toDouble()}
-            .groupBy(
-                keySelector = { it[2] },
-                valueTransform = { it.sliceArray(0..1) }
-            ).mapValues { (_, values) ->
-                val inputValues = values.map { it[0] }.toDoubleArray()
-                val positions = values.map { it[1] }.toDoubleArray()
-                Pair(inputValues, positions)
-            }.toSortedMap()
-        return getQuantileValues(quantiles)
-            .flatMap { (str, indices) -> indices.map { index -> str to index }}
-            .sortedBy { pair -> pair.second }
-            .map { pair -> pair.first }
-            .toList()
-    }
-
-    private fun getQuantileValues(numericValues: SortedMap<Double, Pair<DoubleArray, DoubleArray>>): Map<String, DoubleArray> {
-        val quantileValues = HashMap<String, DoubleArray>()
+    override fun getQuantileValues(numericValues: SortedMap<Int, Pair<IntArray, List<Double>>>): Map<String, IntArray> {
+        val quantileValues = HashMap<String, IntArray>()
         for (i in 0 until numericValues.size) {
-            val values = numericValues[i.toDouble()]?.first
+            val values = numericValues[i]?.second
                 ?: throw IllegalArgumentException("The required quantile is not defined in the input")
-            val indices = numericValues[i.toDouble()]?.second
+            val indices = numericValues[i]?.first
                 ?: throw IllegalArgumentException("The required quantile is not defined in the input")
-            val nextVal = numericValues[i + 1.0]?.first?.min()
-            val prevVal = numericValues[i - 1.0]?.first?.max()
+            val nextVal = numericValues[i + 1]?.second?.min()
+            val prevVal = numericValues[i - 1]?.second?.max()
             val upperBound = if (nextVal != null) {
                 (values.max() + nextVal) / 2
             } else values.max()
-            val lowerBound = if(prevVal != null) {
+            val lowerBound = if (prevVal != null) {
                 (values.min() + prevVal) / 2
             } else values.min()
             if (i == 0) {
                 quantileValues["<= $upperBound"] = indices
-            } else if(i == numericValues.size-1) {
+            } else if (i == numericValues.size - 1) {
                 quantileValues[">= $lowerBound"] = indices
             } else {
                 quantileValues["[$lowerBound, $upperBound]"] = indices
@@ -54,4 +29,13 @@ class GeneralizationNumeric: Generalization() {
         }
         return quantileValues
     }
+
+    override fun convertValues(index: Int, value: Any): Pair<Int, Double> {
+        when (value) {
+            is Double -> return Pair(index, value)
+            is Int -> return Pair(index, value.toDouble())
+            else -> throw IllegalArgumentException("Numeric Generalization was requested but the input $value is not numeric")
+        }
+    }
+
 }
