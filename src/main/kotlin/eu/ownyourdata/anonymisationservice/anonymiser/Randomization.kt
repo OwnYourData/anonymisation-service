8d46662a72b05957280bc0c9572eded282b76cab
paul2803-byte <feichtenschlager10@icloud.com>
2025-01-29 21:44:46 +0100

    Numeric Randomization

--- src/main/kotlin/eu/ownyourdata/anonymisationservice/anonymiser/Randomization.kt
+++ src/main/kotlin/eu/ownyourdata/anonymisationservice/anonymiser/Randomization.kt
@@ -2,5 +2,9 @@ package eu.ownyourdata.anonymisationservice.anonymiser
 
 abstract class Randomization: Anonymiser {
 
+    companion object {
+        const val PRIVACY_FACTOR = 1
+    }
+
     // TODO include general functions for randomization
 }
--- src/main/kotlin/eu/ownyourdata/anonymisationservice/anonymiser/RandomizationNumeric.kt
+++ src/main/kotlin/eu/ownyourdata/anonymisationservice/anonymiser/RandomizationNumeric.kt
@@ -1,9 +1,32 @@
 package eu.ownyourdata.anonymisationservice.anonymiser
 
+import java.lang.IllegalArgumentException
+import java.util.Random
+import kotlin.math.pow
+import kotlin.math.sqrt
+
 class RandomizationNumeric: Randomization() {
 
+    /**
+     * Grad der Randomization hängt von der Samplesize ab
+     * Es soll nicht mehr möglich sein einzelne Datenpunkte zu identifizieren
+     * Eventuell noch eine unterschiedliche Implementierung für ratio und intervalue skaliert
+     */
+
     override fun anonymise(values: MutableList<Any>): List<Any> {
-        // TODO: implement randomization for integer values
-        return values
+        val numericValues = values.stream().map { value ->
+            when (value) {
+                is Double -> value
+                is Int -> value.toDouble()
+                else -> throw IllegalArgumentException("Numeric Generalization was requested but the input $value is not numeric")
+            }
+        }.toList().toDoubleArray()
+        val mean = numericValues.average()
+        val sd = sqrt(numericValues.map { (it - mean).pow(2) }.average())
+        return numericValues.map { v -> addRandomNoise(sd, values.size, v) }
+    }
+
+    private fun addRandomNoise(sd: Double, size: Int, value: Double): Double{
+        return value + Random().nextGaussian() * PRIVACY_FACTOR * (sd / sqrt(size.toDouble()))
     }
 }
